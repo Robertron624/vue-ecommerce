ec9296f117d1b753d9ae4a871cfcd7b3b03a726d
Roberto Ramirez <robert.624@hotmail.com>
2023-07-12 16:36:51 -0500

    added header with logo and minicart icon

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,48 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+import TopHeader from './components/topHeader.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <TopHeader />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  body {
+    margin: 0;
+    font-family: sans-serif, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
+      "Fira Sans", "Droid Sans", "Helvetica Neue";
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  img {
+    max-width: 100%;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
+  button {
+    cursor: pointer;
+    color: #fff;
+  }
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
+
+  .products {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
     flex-wrap: wrap;
+    justify-content: center;
+    margin: 0 auto;
+    max-width: 1200px;
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/productGrid.vue
@@ -0,0 +1,127 @@
+<script>
+
+// Import the mock data
+
+import mockData from '../mockData.js';
+
+
+// create thee arrays for the three columns
+
+let firstColumn = [];
+let secondColumn = [];
+let thirdColumn = [];
+
+// loop through the mockData and push the data to the arrays
+
+for (let i = 0; i < mockData.length; i++) {
+    if (i % 3 === 0) {
+        firstColumn.push(mockData[i]);
+    } else if (i % 3 === 1) {
+        secondColumn.push(mockData[i]);
+    } else {
+        thirdColumn.push(mockData[i]);
+    }
+}
+
+
+
+export default {
+    name: 'ProductGrid',
+    data() {
+        return {
+            firstColumn: firstColumn,
+            secondColumn: secondColumn,
+            thirdColumn: thirdColumn,
+        }
+    },
+    methods: {
+        // when the user clicks on a product, navigate to the product page
+        goToProduct(product) {
+            this.$router.push({ name: 'product', params: { id: product.id } });
+        }
+    },
+
+    computed: {
+        // get the products from the store
+        allProducts() {
+            return this.$store.state.allProducts;
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="container">
+
+    <h1>
+        Store
+    </h1>
+        <div class="product-grid">
+            <!-- show the products in three columns  -->
+
+            <div class="colum">
+                <div class="product" v-for="product in firstColumn" :key="product.id">
+                    <img :src="product.image" alt="product.name">
+                </div>
+            </div>
+
+            <div class="colum">
+                <div class="product" v-for="product in secondColumn" :key="product.id">
+                    <img :src="product.image" alt="product.name">
+                </div>
+            </div>
+
+            <div class="colum">
+                <div class="product" v-for="product in thirdColumn" :key="product.id">
+                    <img :src="product.image" alt="product.name">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+
+.container {
+    max-width: 500px;
+
+    h1 {
+        font-size: 2rem;
+        border-bottom: 1px solid rgb(240	232	241	);
+        color: rgb(213, 158, 221);
+    }
+
+    .product-grid {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+       
+
+        .colum {
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            height: fit-content;
+        }
+
+        .product {
+            height: fit-content;
+            border: 1px solid rgb(240	232	241	);
+            padding: 1rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+
+            img {
+                max-width: 100%;
+                width: 100px;
+                object-fit: cover;
+            }
+        }
+    }
+
+}
+
+</style>
