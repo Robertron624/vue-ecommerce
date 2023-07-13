14a4e3388a144b77fba9d3aeb0f421dfd63cb1a5
Roberto Ramirez <robert.624@hotmail.com>
2023-07-13 17:29:09 -0500

    refactor: removed unnecessary components

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createStore } from 'vuex'
 import App from './App.vue'
-import router from './router'
+
 
 const app = createApp(App)
-
-app.use(router)
 
 import products from './mockData.js'
 
